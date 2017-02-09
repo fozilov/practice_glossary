f3f5e550d64921490de3fcfa251fef22e878e96a
fozilov <fozilov@sansan.com>
2017-02-09 15:02:59 +0900

    added materials ui

--- app/models/glossary.rb
+++ app/models/glossary.rb
@@ -1,2 +1,3 @@
 class Glossary < ApplicationRecord
+  has_and_belongs_to_many :labels
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170209024336) do
+ActiveRecord::Schema.define(version: 20170209024435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20170209024336) do
     t.string   "label_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.text     "request_reason"
+    t.string   "requester"
+    t.text     "decision_reason"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
